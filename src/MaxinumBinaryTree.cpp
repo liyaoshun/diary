da6ffe3567d6b9125ae10b1effd48f05f3d2c3ee
yaoshun <yaoshun.li@cloudminds.com>
2021-03-15 16:09:34 +0800

    add rebuild_btree_from_pre_inorder

--- src/MaxinumBinaryTree.cpp
+++ src/MaxinumBinaryTree.cpp
@@ -19,6 +19,7 @@ struct TreeNode{
     TreeNode(int x, TreeNode * left, TreeNode * right):val(x),left(left),right(right){};
 };
 
+
 #pragma region  最大二叉树构建实现代码
 TreeNode * buildRoot(int lo, int hi, int arr[])
 {
@@ -57,12 +58,127 @@ TreeNode *constructMaxinumBinaryTree(int len, int arr[])
     int hi = len - 1;
     return buildRoot(lo, hi, arr);
 }
+#pragma endregion
+
+
+#pragma region  遍历最大二叉树
+
+//前序遍历
+void preorder_call(TreeNode * root)
+{
+    if (nullptr == root)
+    {
+        return;
+    }
+    cout<<root->val<<endl;
+    preorder_call(root->left);
+    preorder_call(root->right);
+}
+//中序遍历
+void midorder_call(TreeNode * root)
+{
+    if (nullptr == root)
+    {
+        return;
+    }
+    cout<<root->val<<endl;
+    midorder_call(root->left);
+    midorder_call(root->right);
+}
+//后序遍历
+void postorder_call(TreeNode * root)
+{
+    if (nullptr == root)
+    {
+        return;
+    }
+    cout<<root->val<<endl;
+    postorder_call(root->left);
+    postorder_call(root->right);
+}
+#pragma endregion
+
+
+#pragma region  根据遍历结果来重构二叉树
+
+//通过前序、中序结果重构二叉树
+// 前序遍历 preorder = [3,9,20,15,7]
+// 中序遍历 inorder = [9,3,15,20,7]
+TreeNode * engine_rebuild_btree(int preorder[], int inorder[],int lo, int hi, int &pre_index)
+{
+    if (lo>hi)
+    {
+        pre_index -= 1;
+        return nullptr;
+    }
+    
+    int root_value = preorder[pre_index];
+    
+    TreeNode * root = new TreeNode(root_value);
+    //根据root_value将中序（inorder）数组分为左右子树。
+    int index = -1;//index表示在中序中root节点的数组下标
+    for(int i = lo; i <= hi; i++)
+    {
+        if(root_value == inorder[i])
+        {
+            index = i;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    if (index < 0)
+    {
+        root->left = nullptr;
+        root->right = nullptr;
+        return root;
+    }
+
+    pre_index+=1;
+    int l_lo = lo;
+    int l_hi = index - 1;
+    int r_lo = index + 1;
+    int r_hi = hi;
+
+    root->left  = engine_rebuild_btree(preorder, inorder, l_lo, l_hi, pre_index);
+    pre_index+=1;
+    root->right = engine_rebuild_btree(preorder, inorder, r_lo, r_hi, pre_index);
+    return root;
+
+}
+
+TreeNode * rebuild_btree_from_pre_inorder(int preorder[], int inorder[], int len = 5)
+{
+    if (nullptr == preorder || nullptr == inorder )
+    {
+        return nullptr;
+    }
+    int lo = 0;int hi = len - 1;int pre_index = 0;
+    engine_rebuild_btree(preorder, inorder, lo, hi, pre_index);
+    
+}
+//通过中序、后续结果重构二叉树
+// 中序遍历 inorder = [9,3,15,20,7]
+// 后序遍历 postorder = [9,15,7,20,3]
+void mid_post_rebuild_btree(int preorder[], int inorder[])
+{
+
+}
 
 #pragma endregion
 
 int main(void)
 {
-    int a[6] = {3,2,1,6,0,5};
-    TreeNode * T = constructMaxinumBinaryTree(6, a);
+    // int a[6] = {3,2,1,6,0,5};
+    // TreeNode * T = constructMaxinumBinaryTree(6, a);
+    // preorder_call(T);
+    int preorder[5] = {3,9,20,15,7};
+    int inorder[5] = {9,3,15,20,7};
+    int postorder[5] = {9,15,7,20,3};
+    TreeNode * T = rebuild_btree_from_pre_inorder(preorder, inorder, 5);
+
     return 0;
 }
